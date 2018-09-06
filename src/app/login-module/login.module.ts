5277e9147b3ad33b103647197f745f831518b742
Ajit Soman <ajitdev381@gmail.com>
2018-09-06 17:49:07 +0530

    added login and signup ui

--- src/app/login-module/login.module.ts
+++ src/app/login-module/login.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { LoginRouteModule } from './login.route.module';
 import { LoginComponent } from './login.component';
-import { MatFormFieldModule, MatInputModule } from '../../../node_modules/@angular/material';
+import { MatFormFieldModule, MatInputModule,MatTabsModule } from '../../../node_modules/@angular/material';
 
 @NgModule({
     imports: [
         LoginRouteModule,
         MatFormFieldModule,
-        MatInputModule
+        MatInputModule,
+        MatTabsModule
     ],
     declarations: [
         LoginComponent
