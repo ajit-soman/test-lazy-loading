fabfb76754d4cd5e8b40a53f48debb17408c6288
Ajit Soman <ajitdev381@gmail.com>
2018-09-06 18:22:57 +0530

    added side nav bar

--- src/app/dashboard-module/dashboard.module.ts
+++ src/app/dashboard-module/dashboard.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { DashboardRouteModule } from './dashboard.route.module';
 import { DashboardComponent } from './dashboard.component';
+import {MatSidenavModule} from '@angular/material';
 
 @NgModule({
     imports: [
         DashboardRouteModule,
+        MatSidenavModule
     ],
     declarations: [
         DashboardComponent
